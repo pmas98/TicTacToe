aee705ec0cc01109b4f43744f5c5dc497169ad30
Pedro Gabriel <pmas98@hotmail.com>
2022-11-07 15:45:43 -0300

    Updates

---
+++ precache-manifest.9342ba756ece390e70f36d25ad5cb4ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a05cf3a7b311d4d38f6e",
+    "url": "/TicTacToe/static/css/main.f6431cb8.chunk.css"
+  },
+  {
+    "revision": "a05cf3a7b311d4d38f6e",
+    "url": "/TicTacToe/static/js/main.a05cf3a7.chunk.js"
+  },
+  {
+    "revision": "7b2e37571c17e8d4fdd1",
+    "url": "/TicTacToe/static/js/1.7b2e3757.chunk.js"
+  },
+  {
+    "revision": "15e80ec1ac795034716e",
+    "url": "/TicTacToe/static/js/2.15e80ec1.chunk.js"
+  },
+  {
+    "revision": "93ef6a3323a85c282214",
+    "url": "/TicTacToe/static/js/runtime~main.93ef6a33.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/TicTacToe/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "ae4d9058be463c95ea36c019a6b612aa",
+    "url": "/TicTacToe/index.html"
+  }
+];
